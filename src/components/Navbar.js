cdfd4cf05ecc6fc4e8e98ba23e7b8cba4961b682
inesmariao <inesmao.misena@gmail.com>
2024-07-26 06:52:22 -0500

    upload projects image

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <h1>Navbar</h1>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <Link href="/" className="navbar-brand">
+          Portfolio
+        </Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link href="/blog" className="nav-link active" aria-current="page" >
+                Blog
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/github" className="nav-link" >
+                GitHub
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   )
 }
 
---
+++ src/pages/profile.js
@@ -0,0 +1,115 @@
+
+export const skills = [
+  {
+    skill: "JavaScript",
+    percentage: 100,
+  },
+  {
+    skill: "TypeScript",
+    percentage: 100,
+  },
+  {
+    skill: "Node.js",
+    percentage: 100,
+  },
+  {
+    skill: "Next.js",
+    percentage: 100,
+  },
+  {
+    skill: "MongoDB",
+    percentage: 100,
+  },
+  {
+    skill: "React.js",
+    percentage: 100,
+  },
+  {
+    skill: "Bootstrap",
+    percentage: 100,
+  },
+];
+
+export const experiences = [
+  {
+    title: "Software developer at Microsoft",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem officiis fugiat vel animi aliquam inventore rem. Quo laudantium temporibus cupiditate. Aut?",
+    from: 2000,
+    to: 2005,
+  },
+  {
+    title: "Software developer at Google",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem officiis fugiat vel animi aliquam inventore rem. Quo laudantium temporibus cupiditate. Aut?",
+    from: 2005,
+    to: 2007,
+  },
+  {
+    title: "Software developer at SpaceX",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem officiis fugiat vel animi aliquam inventore rem. Quo laudantium temporibus cupiditate. Aut?",
+    from: 2010,
+  },
+];
+
+export const projects = [
+  {
+    name: "Awesome Website 1",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "portfolio1.png",
+  },
+  {
+    name: "Awesome Website 2",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "portfolio1.png",
+  },
+  {
+    name: "Awesome Website 3",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "portfolio1.png",
+  },
+  {
+    name: "Awesome Website 4",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "portfolio1.png",
+  },
+  {
+    name: "Awesome Website 5",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "portfolio1.png",
+  },
+  {
+    name: "Awesome Website 6",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "portfolio1.png",
+  },
+];
+
+export const posts = [
+  {
+    title: "React",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    imageURL: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  },
+  {
+    title: "Angular",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    imageURL: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  },
+  {
+    title: "Nextjs",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    imageURL:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  },
+];
